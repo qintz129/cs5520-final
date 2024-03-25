b701e2a8533ee040393ab7972dc68204dac4059f
qintz129 <sophieqin51244@163.com>
2024-03-24 20:43:00 -0700

    fetch data from history collection

--- bookcosmos/screens/History.js
+++ bookcosmos/screens/History.js
@@ -1,7 +1,54 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useEffect, useState} from 'react' 
+import { collection, onSnapshot, query, where, getDocs} from "firebase/firestore"; 
+import { database, auth} from "../firebase-files/firebaseSetup";
 
-export default function History() {
+export default function History() {  
+  const [history, setHistory] = useState([]);
+  useEffect(() => { 
+    const fromQuery = query(collection(database, "history"), where("fromUser", "==", auth.currentUser.uid)); 
+    const toQuery = query(collection(database, "history"),  where("toUser", "==", auth.currentUser.uid));  
+    let fromArray = []; 
+    let toArray = []; 
+    const updateCombinedHistory = () => { 
+      const historyMap = new Map(); 
+      fromArray.forEach((item) => historyMap.set(item.id, item)); 
+      toArray.forEach((item) => historyMap.set(item.id, item)); 
+      const combinedArray = Array.from(historyMap.values()); 
+      setHistory(combinedArray); 
+    }
+    // set up a listener to get realtime data from firestore - only after the first render
+    const unsubscribeFrom = onSnapshot(
+      fromQuery, (querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          fromArray.push({ ...doc.data(), id: doc.id });
+        }); 
+        updateCombinedHistory();
+      },  
+      (error) => { 
+        Alert.alert(error.message); 
+      }
+    ); 
+
+  const unsubscribeTo = onSnapshot(
+    toQuery, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        toArray.push({ ...doc.data(), id: doc.id });
+      }); 
+      updateCombinedHistory();
+    }, 
+    (error) => { 
+      Alert.alert(error.message); 
+    }
+  );
+    return () => {
+      unsubscribeFrom(); 
+      unsubscribeTo();
+    };
+  }, []); 
+  
+  console.log(history);
+
   return (
     <View>
       <Text>History</Text>
