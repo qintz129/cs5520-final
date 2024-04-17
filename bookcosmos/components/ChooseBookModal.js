eb8e972a8ecbd0045fe0c022811b451165897721
Hanna Zhou <zhou.hanyi@northeastern.edu>
2024-04-17 00:49:24 -0700

    add book detail and choose book modal styles

--- bookcosmos/components/ChooseBookModal.js
+++ bookcosmos/components/ChooseBookModal.js
@@ -7,10 +7,11 @@ import {
   writeToDB,
   updateToDB,
   createExchangeRequest,
-} from "../firebase-files/firestoreHelper"; 
-import * as Notifications from 'expo-notifications';  
-import {throttle} from 'lodash'; 
-import { useUser } from "../hooks/UserContext"; 
+} from "../firebase-files/firestoreHelper";
+import * as Notifications from "expo-notifications";
+import { throttle } from "lodash";
+import { useUser } from "../hooks/UserContext";
+import { useCustomFonts } from "../Fonts";
 
 // ChooseBookModal component to display a modal to choose a book for exchange
 export default function ChooseBookModal({
@@ -23,36 +24,41 @@ export default function ChooseBookModal({
   setRequestSent,
 }) {
   const [books, setBooks] = useState([]);
-  const [selectedBookId, setSelectedBookId] = useState(null);  
-  const {userInfo} = useUser();
+  const [selectedBookId, setSelectedBookId] = useState(null);
+  const { userInfo } = useUser();
 
-  async function scheduleNotification() {  
-    try {  
-        if (!userInfo.notification) {  
-          console.log("User has disabled notifications");
-          return; 
-        }
-        const id = await Notifications.scheduleNotificationAsync({ 
-            content: { 
-                title: "Book Exchange Request",  
-                body: "Check your book exchange request status!", 
-                data: {screen: "Requests"},
-            }, 
-            // Set the notification to be sent after 3 seconds for testing purposes
-            trigger:{seconds: 3}, 
-        });
-    } catch (error) { 
-        console.log(error)
-    } 
-} 
+  const { fontsLoaded } = useCustomFonts();
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
 
-const scheduleThrottledNotification = throttle(
+  async function scheduleNotification() {
+    try {
+      if (!userInfo.notification) {
+        console.log("User has disabled notifications");
+        return;
+      }
+      const id = await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Book Exchange Request",
+          body: "Check your book exchange request status!",
+          data: { screen: "Requests" },
+        },
+        // Set the notification to be sent after 3 seconds for testing purposes
+        trigger: { seconds: 3 },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const scheduleThrottledNotification = throttle(
     () => {
       console.log("Scheduling notification...");
       scheduleNotification();
     },
-    86400000, // 24 hours  
-    { 'trailing': false }
+    86400000, // 24 hours
+    { trailing: false }
   );
 
   useEffect(() => {
@@ -113,12 +119,12 @@ const scheduleThrottledNotification = throttle(
           });
 
         // Update book status to indicate it is in exchange
-        await updateBookStatus(selectedBookId); 
+        await updateBookStatus(selectedBookId);
 
         // Set the requestSent state to true
-        setRequestSent(true); 
+        setRequestSent(true);
 
-        // Schedule a notification for the user 
+        // Schedule a notification for the user
         scheduleThrottledNotification();
 
         // Close the modal
@@ -151,18 +157,24 @@ const scheduleThrottledNotification = throttle(
                 ]}
               >
                 <CustomButton onPress={() => handleSelectBook(item.id)}>
-                  <Text>{item.bookName}</Text>
+                  <Text style={styles.bookNameText}>{item.bookName}</Text>
                 </CustomButton>
               </View>
             )}
             keyExtractor={(item) => item.id.toString()}
           />
           <View style={styles.buttonContainer}>
-            <CustomButton onPress={onRequestClose}>
-              <Text>Cancel</Text>
+            <CustomButton
+              onPress={onRequestClose}
+              customStyle={styles.cancelButtonStyle}
+            >
+              <Text style={styles.buttonText}>Cancel</Text>
             </CustomButton>
-            <CustomButton onPress={handleConfirm}>
-              <Text>Confirm</Text>
+            <CustomButton
+              onPress={handleConfirm}
+              customStyle={styles.confirmButtonStyle}
+            >
+              <Text style={styles.buttonText}>Confirm</Text>
             </CustomButton>
           </View>
         </View>
@@ -187,8 +199,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   modalTitle: {
-    fontSize: 18,
+    fontSize: 20,
     marginBottom: 10,
+    fontFamily: "SecularOne_400Regular",
   },
   item: {
     padding: 10,
@@ -203,5 +216,27 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     width: "100%",
     marginTop: 20,
+  },
+  bookNameText: {
+    fontSize: 18,
+    fontFamily: "Molengo_400Regular",
+    textAlign: "center",
+  },
+  cancelButtonStyle: {
+    backgroundColor: "#ff5c5c",
+    width: "40%",
+    height: 40,
+    borderRadius: 10,
+  },
+  confirmButtonStyle: {
+    backgroundColor: "#55c7aa",
+    width: "40%",
+    height: 40,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "#f5f5f5",
+    fontSize: 16,
+    fontFamily: "SecularOne_400Regular",
   },
 });
--- bookcosmos/screens/BookDetail.js
+++ bookcosmos/screens/BookDetail.js
@@ -1,4 +1,13 @@
-import { StyleSheet, Text, View, Image, Alert, Linking, ActivityIndicator,ScrollView} from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Alert,
+  Linking,
+  ActivityIndicator,
+  ScrollView,
+} from "react-native";
 import React, { useState, useEffect } from "react";
 import { doc, getDoc, getDocs, collection } from "firebase/firestore";
 import { database, auth } from "../firebase-files/firebaseSetup";
@@ -7,9 +16,10 @@ import ChooseBookModal from "../components/ChooseBookModal";
 import AntDesign from "react-native-vector-icons/AntDesign";
 import { ref, getDownloadURL } from "firebase/storage";
 import { storage } from "../firebase-files/firebaseSetup";
-import { Entypo } from "@expo/vector-icons"; 
-import { googleApi } from "@env"; 
-
+import { Entypo } from "@expo/vector-icons";
+import { googleApi } from "@env";
+import { useCustomFonts } from "../Fonts";
+import { FontAwesome } from "@expo/vector-icons";
 
 // BookDetail component to display the details of a book
 export default function BookDetail({ route, navigation }) {
@@ -17,6 +27,8 @@ export default function BookDetail({ route, navigation }) {
   const [author, setAuthor] = useState("");
   const [bookImageURI, setBookImageURI] = useState("");
   const [description, setDescription] = useState("");
+  const [ownerAvatarURI, setOwnerAvatarURI] = useState("");
+  const [ownerAvatar, setOwnerAvatar] = useState(null);
   const [ownerName, setOwnerName] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
   const [requestSent, setRequestSent] = useState(false);
@@ -24,7 +36,12 @@ export default function BookDetail({ route, navigation }) {
   const { bookId, ownerId, distance } = route.params;
   const [rating, setRating] = useState(0);
   const [isLoading, setLoading] = useState(false);
-  const [bookAvatar, setBookAvatar] = useState(null); 
+  const [bookAvatar, setBookAvatar] = useState(null);
+
+  const { fontsLoaded } = useCustomFonts();
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
 
   useEffect(() => {
     let bookData;
@@ -52,13 +69,14 @@ export default function BookDetail({ route, navigation }) {
       }
     };
 
-    // Fetch the owner name from the database by ownerId
+    // Fetch the owner avatar and name from the database by ownerId
     const fetchOwnerName = async () => {
       try {
         const docRef = doc(database, "users", bookData.owner);
         const docSnap = await getDoc(docRef);
         if (docSnap.exists()) {
           const userData = docSnap.data();
+          setOwnerAvatarURI(userData.image);
           setOwnerName(userData.name);
         } else {
           setOwnerName("Unknown");
@@ -131,88 +149,148 @@ export default function BookDetail({ route, navigation }) {
           console.error("Failed to load image:", error);
         });
     }
-  }, [bookImageURI]); 
+  }, [bookImageURI]);
 
-
-
-
-const fetchBookDetails = async (name, author) => {
-  const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(name)}+inauthor:${encodeURIComponent(author)}&key=${googleApi}`;
-  console.log('Fetching book details from:', url);
-  try {
-    const response = await fetch(url);
-    const json = await response.json();
-
-    if (json.totalItems > 0 && json.items[0].volumeInfo.infoLink) {
-      // Assuming you want to open the first result
-      const bookUrl = json.items[0].volumeInfo.infoLink;
-      Linking.openURL(bookUrl);
-    } else {
-      Alert.alert( "Sorry, no books found with the given name and author.");
+  useEffect(() => {
+    if (ownerAvatarURI) {
+      const imageRef = ref(storage, ownerAvatarURI);
+      getDownloadURL(imageRef)
+        .then((url) => {
+          setOwnerAvatar(url);
+        })
+        .catch((error) => {
+          console.error("Failed to load image:", error);
+        });
     }
-  } catch (error) {
-    console.error('Failed to fetch book details:', error);
-  }
-};
+  }, [ownerAvatarURI]);
 
+  const fetchBookDetails = async (name, author) => {
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
+      name
+    )}+inauthor:${encodeURIComponent(author)}&key=${googleApi}`;
+    console.log("Fetching book details from:", url);
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+
+      if (json.totalItems > 0 && json.items[0].volumeInfo.infoLink) {
+        // Assuming you want to open the first result
+        const bookUrl = json.items[0].volumeInfo.infoLink;
+        Linking.openURL(bookUrl);
+      } else {
+        Alert.alert("Sorry, no books found with the given name and author.");
+      }
+    } catch (error) {
+      console.error("Failed to fetch book details:", error);
+    }
+  };
 
   return (
-    <View> 
+    <View>
       <ScrollView>
-      {isLoading ? (
-        <ActivityIndicator size="large" color="#0000ff" />
-      ) : (
-        <View style={styles.container}>
-          {bookAvatar ? (
-            <Image source={{ uri: bookAvatar }} style={styles.image} />
-          ) : (
-            <AntDesign name="picture" size={50} color="grey" />
-          )} 
-          <View style={styles.distanceContainer}>
-            <Entypo name="location-pin" size={24} color="black" />
-            <Text>{distance} km</Text>
-          </View>
-          <Text style={styles.title}>{bookName}</Text>
-          <Text style={styles.author}>{author}</Text>
-          <Text>{description}</Text>
-          <View>
-            <CustomButton
-              onPress={() =>
-                navigation.navigate("Other User Profile", {
-                  ownerId: ownerId,
-                  ownerName: ownerName,
-                  rating: rating,
-                })
-              }
-            >
-              <Text>User: {ownerName}</Text>
-              {rating > 0 && <Text>Rating: {rating}</Text>}
-            </CustomButton>
-          </View>
-          <View style={styles.googleBooks}>
-            <CustomButton onPress={() => fetchBookDetails(bookName, author)}>
-              <Text>See more information from Google Books</Text>
-            </CustomButton>
-          </View>
-          <View style={styles.buttonContainer}>
-            {!requestSent && bookStatus === "free" && (
-              <CustomButton onPress={handleSendRequest}>
-                <Text>Send Request</Text>
-              </CustomButton>
+        {isLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <View style={styles.container}>
+            {bookAvatar ? (
+              <Image source={{ uri: bookAvatar }} style={styles.image} />
+            ) : (
+              <AntDesign name="picture" size={50} color="grey" />
             )}
+            <View style={styles.bookInfoContainer}>
+              <Text style={styles.titleText}>{bookName}</Text>
+              <Text style={styles.authorText}>{author}</Text>
+              <Text style={styles.descriptionText}>{description}</Text>
+            </View>
+            <View style={styles.googleBooks}>
+              <CustomButton
+                onPress={() => fetchBookDetails(bookName, author)}
+                customStyle={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  columnGap: 10,
+                }}
+              >
+                <AntDesign name="google" size={24} color="black" />
+                <Text style={styles.googleButtonText}>
+                  See more information from Google Books
+                </Text>
+              </CustomButton>
+            </View>
+            <View style={styles.userInfoContainer}>
+              <View style={styles.userButtonContainer}>
+                <CustomButton
+                  onPress={() =>
+                    navigation.navigate("Other User Profile", {
+                      ownerId: ownerId,
+                      ownerName: ownerName,
+                      rating: rating,
+                    })
+                  }
+                  customStyle={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    columnGap: 10,
+                  }}
+                >
+                  <View style={styles.userContainer}>
+                    {ownerAvatar ? (
+                      <Image
+                        source={{ uri: ownerAvatar }}
+                        style={{ width: 50, height: 50, borderRadius: 50 }}
+                      />
+                    ) : (
+                      <AntDesign name="user" size={50} color="grey" />
+                    )}
+                    <View style={styles.userTextContainer}>
+                      <Text style={styles.userText}>{ownerName}</Text>
+                      {rating > 0 && (
+                        <View style={styles.userRatingContainer}>
+                          <FontAwesome name="star" size={20} color="#fdcc0d" />
+                          <Text style={styles.ratingText}>
+                            Rating: {rating}
+                          </Text>
+                        </View>
+                      )}
+                    </View>
+                  </View>
+                </CustomButton>
+              </View>
+              <View style={styles.divider}></View>
+              <View style={styles.locationContainer}>
+                <Entypo name="location-pin" size={24} color="#55c7aa" />
+                <Text style={styles.distanceText}>{distance} km</Text>
+              </View>
+            </View>
+            <View style={styles.buttonContainer}>
+              {!requestSent && bookStatus === "free" && (
+                <CustomButton
+                  onPress={handleSendRequest}
+                  customStyle={{
+                    backgroundColor: "#55c7aa",
+                    width: 200,
+                    height: 50,
+                    borderRadius: 10,
+                    alignSelf: "center",
+                  }}
+                >
+                  <Text style={styles.buttonText}>Send Request</Text>
+                </CustomButton>
+              )}
+            </View>
+            <ChooseBookModal
+              visible={modalVisible}
+              onRequestClose={() => setModalVisible(false)}
+              onSelectBook={handleSelectBook}
+              fromUserId={auth.currentUser.uid}
+              requestedBookId={bookId}
+              toUserId={ownerId}
+              requestSent={requestSent}
+              setRequestSent={setRequestSent}
+            />
           </View>
-          <ChooseBookModal
-            visible={modalVisible}
-            onRequestClose={() => setModalVisible(false)}
-            onSelectBook={handleSelectBook}
-            fromUserId={auth.currentUser.uid}
-            requestedBookId={bookId}
-            toUserId={ownerId}
-            requestSent={requestSent}
-            setRequestSent={setRequestSent}
-          />
-        </View>
-      )} 
+        )}
       </ScrollView>
     </View>
   );
@@ -224,33 +302,107 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   image: {
-    width: 200,
-    height: 250,
+    width: "100%",
+    height: 400,
     borderRadius: 10,
   },
-  title: {
-    fontSize: 25,
-    marginBottom: 10,
+  bookInfoContainer: {
+    padding: 10,
+  },
+  titleText: {
+    fontSize: 30,
+    marginBottom: 5,
     fontWeight: "bold",
+    fontFamily: "SecularOne_400Regular",
+    marginTop: 10,
+    textAlign: "center",
   },
-  author: {
+  authorText: {
+    marginTop: 5,
+    marginBottom: 15,
+    fontSize: 25,
+    fontFamily: "Molengo_400Regular",
+    textAlign: "center",
+  },
+  descriptionText: {
+    fontSize: 18,
     marginBottom: 10,
-    fontSize: 20,
+    fontFamily: "Catamaran_400Regular",
+    textAlign: "justify",
   },
-  distanceContainer: {
+  userInfoContainer: {
+    marginTop: 20,
     flexDirection: "row",
     alignItems: "center",
-    marginTop: 10,
+    justifyContent: "space-evenly",
+    borderWidth: 1,
+    borderColor: "lightgrey",
+    padding: 10,
+    borderRadius: 10,
+  },
+  userButtonContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  userContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    columnGap: 20,
+  },
+  userTextContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  userText: {
+    fontSize: 18,
+    fontFamily: "SecularOne_400Regular",
+    textAlign: "center",
+  },
+  userRatingContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    columnGap: 5,
+  },
+  ratingText: {
+    fontSize: 18,
+    fontFamily: "SecularOne_400Regular",
+    textAlign: "center",
+  },
+  divider: {
+    height: 50,
+    width: 1,
+    backgroundColor: "lightgrey",
+  },
+  locationContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  distanceText: {
+    fontSize: 18,
+    fontFamily: "SecularOne_400Regular",
+    textAlign: "center",
   },
   buttonText: {
-    color: 'white',
-    fontSize: 16,
-    fontWeight: 'bold',
+    color: "#f5f5f5",
+    fontSize: 18,
+    fontFamily: "SecularOne_400Regular",
   },
   googleBooks: {
-    marginTop: 20,
+    borderWidth: 1,
+    borderColor: "lightgrey",
+    padding: 10,
+    borderRadius: 10,
+  },
+  googleButtonText: {
+    fontSize: 16,
+    fontFamily: "SecularOne_400Regular",
+    color: "black",
   },
   buttonContainer: {
-    marginTop: 20,
+    marginTop: 10,
   },
 });
